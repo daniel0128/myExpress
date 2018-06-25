c3203282e72069af8b2469463d78fd03e7e4212c
daniel0128 <survey600@qq.com>
2018-06-25 00:00:09 -0700

    lession 2 update

--- example/index.js
+++ example/index.js
@@ -1,3 +1,20 @@
 const express = require('../index.js');
 const app = express();
+
+app.get((req, res) => {
+	res.end('You send GET request.');
+});
+
+app.post((req, res) => {
+	res.end('You send POST request');
+});
+
+app.put((req, res) => {
+	res.end('You send PUT request');
+});
+
+app.delete((req, res) => {
+	res.end('You send DELETE request');
+});
+
 app.listen(5000);
