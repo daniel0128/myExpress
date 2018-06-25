aff7eabe2ee29da51f443972110ba3c3d0fad2ed
daniel0128 <survey600@qq.com>
2018-06-25 16:24:44 -0700

    lesson 3 middle ware and next

--- example/index.js
+++ example/index.js
@@ -1,20 +1,38 @@
 const express = require('../index.js');
 const app = express();
 
-app.get((req, res) => {
-	res.end('You send GET request.');
+// lesson 2
+// app.get((req, res) => {
+// 	res.end('You send GET request.');
+// });
+//
+// app.post((req, res) => {
+// 	res.end('You send POST request');
+// });
+//
+// app.put((req, res) => {
+// 	res.end('You send PUT request');
+// });
+//
+// app.delete((req, res) => {
+// 	res.end('You send DELETE request');
+// });
+
+// lesson 3
+app.get((req, res, next) => {
+	req.user = {
+		name: 'fool'
+	};
+	next();
 });
 
-app.post((req, res) => {
-	res.end('You send POST request');
-});
-
-app.put((req, res) => {
-	res.end('You send PUT request');
-});
-
-app.delete((req, res) => {
-	res.end('You send DELETE request');
+app.get((req, res, next) => {
+	req.article = {
+		title: 'bar'
+	};
+	next();
+}, (req, res, next) => {
+	res.end(`User name is ${req.user.name}, and Article title is ${req.article.title}.`);
 });
 
 app.listen(5000);
