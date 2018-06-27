1f50e377abda0b0574f60a9120eef94fdec7f287
daniel0128 <survey600@qq.com>
2018-06-27 14:14:25 -0700

    finish following

--- example/index.js
+++ example/index.js
@@ -77,4 +77,33 @@ app.get('/article/:title', (req, res, next) => {
 	res.end(`Welcome, the article's title is "${req.params.title}"\n`);
 });
 
+// lesson 6
+// curl -X GET http://localhost:5000/foo
+app.get('/foo', function handle1 (req, res, next) {
+	next(new Error('Bang!'));
+}, function handle2 (req, res, next) {
+	res.end('Will not go here');
+}, function handle3 (err, req, res, next) {
+	console.log(`Error Caught! Error message is ${err.message}`);
+	next(err);
+});
+
+app.get('/foo', function (req, res, next) {
+	res.end('Will not go here too');
+});
+
+app.use('/foo', function (req, res, next) {
+	res.end('Will not go here too');
+});
+
+app.get('/foo', function (err, req, res, next) {
+	console.log(err.name);
+	res.end('Will not go here too');
+});
+
+app.use('/foo', function (err, req, res, next) {
+	console.log(`Error Caught! Error message is ${err.message}`);
+	res.end('Go here\n');
+});
+
 app.listen(5000);
